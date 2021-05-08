0c2b92f819596f2e4222f8b704fc4e0746c841b7
AYODEJI <ayodejiv5@gmail.com>
2021-05-08 16:58:14 +0100

    Created Nav and Main components

---
+++ src/components/Main.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Main.module.css";
+import { animated, useSpring, config } from "react-spring";
+import { ChervonLeft } from "./assets/icon/ChervonLeft";
+import { ChervonRight } from "./assets/icon/ChervonRight";
+export default function Main() {
+  const width = window.innerWidth;
+
+  const [props, api] = useSpring(
+    {
+      from: { x: width, opacity: 0 },
+    },
+    []
+  );
+
+  useEffect(() => {
+    api.start({ x: 0, opacity: 1, delay: 0, config: config.molasses });
+  }, []);
+
+  return (
+    <>
+      <div className={styles.left_background}></div>
+      <div className={styles.right_background}></div>
+      <div className={styles.center_image}>
+        <animated.img
+          className={styles.cener_image}
+          style={props}
+          src="https://res.cloudinary.com/victorcodes/image/upload/v1620485694/Dance%20Academy/photo-1596128124912-11e7648a8533-removebg_zmesnc.png"
+          alt=""
+        />
+      </div>
+      <div className={styles.hero}>
+        <h1 className={styles.hug_content}>
+          <span style={{ textAlign: "left" }} className={styles.hero_small}>
+            Feel the
+          </span>
+          <div className={styles.hero_big}>
+            <span>DANCE</span>
+            <span>RHYTHM</span>
+          </div>
+          <span className={styles.hero_small}>inside your soul</span>
+        </h1>
+      </div>
+      <div className={styles.more_info}>
+        <div className={styles.left_info}>
+          <svg
+            width="3"
+            className={styles.svg}
+            height="120"
+            viewBox="0 0 3 120"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="3" height="120" fill="white" />
+          </svg>
+          <div className={styles.left_info_text}>
+            <span>Open master classes</span>
+            <span>09.23.2017</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.controls}>
+        <animated.p style={props}>Comtemporary dance style</animated.p>
+        <div className={styles.gap}>
+          <ChervonLeft />
+          <ChervonRight />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/assets/icon/ChervonLeft.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export const ChervonLeft = () => {
+  return (
+    <svg
+      width="14"
+      height="42"
+      viewBox="0 0 14 42"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12.9329 41.5529C13.4112 41.2767 13.5751 40.6651 13.299 40.1868L2.31414 21.1606L13.299 2.1343C13.5751 1.656 13.4112 1.04441 12.9329 0.768267C12.4546 0.492125 11.843 0.656001 11.5669 1.13429L1.87488e-05 21.1687L0.0141656 21.1769L11.5669 41.1868C11.8431 41.6651 12.4546 41.829 12.9329 41.5529Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
